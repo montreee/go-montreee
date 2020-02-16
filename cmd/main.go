29b9da35490f126e55468631a281067a7e1d1e2b
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2020-02-16 19:53:19 +0100

    -

--- cmd/main.go
+++ cmd/main.go
@@ -1,128 +1,115 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
-var montreeeDirEnv = "MONTREEE"
+const (
+	envMontreeeDir        = "MONTREEE"
+	defaultMontreeeDir    = "."
+	envMontreeeWorkingDir = "MONTREEE_WORKING_DIR"
+	montreeeClasspath     = "/lib/*"
+	montreeeMainClass     = "CLI"
+)
 
-var defaultMontreeeDir = "."
+var (
+	javaHomeDirEnvs = []string{
+		"MONTREEE_JAVA",
+		"MONTREEE_JAVA_HOME",
+		"MONTREEE_JRE",
+		"MONTREEE_JRE_HOME",
+		"MONTREEE_JVM",
+		"MONTREEE_JVM_HOME",
+		"MONTREEE_JDK",
+		"MONTREEE_JDK_HOME",
+		"JAVA",
+		"JAVA_HOME",
+		"JRE",
+		"JRE_HOME",
+		"JVE",
+		"JVM_HOME",
+		"JDE",
+		"JDK_HOME",
+	}
+	javaExecuteables = []string{
+		"/bin/java",
+		"/jre/bin/java",
+	}
+)
 
-var montreeeWorkingDirEnv = "MONTREEE_WORKING_DIR"
-
-var montreeeClasspath = "/lib/*"
-
-var montreeeMainClass = "CLI"
-
-var defaultJavaCmd = "java"
-
-var posibleJavaHomeDirEnvs = []string{
-	"MONTREEE_JAVA",
-	"MONTREEE_JAVA_HOME",
-	"MONTREEE_JRE",
-	"MONTREEE_JRE_HOME",
-	"MONTREEE_JVM",
-	"MONTREEE_JVM_HOME",
-	"MONTREEE_JDK",
-	"MONTREEE_JDK_HOME",
-	"JAVA",
-	"JAVA_HOME",
-	"JRE",
-	"JRE_HOME",
-	"JVE",
-	"JVM_HOME",
-	"JDE",
-	"JDK_HOME",
-}
-
-var posibleJavaExecuteables = []string{
-	"/bin/java",
-	"/jre/bin/java",
-}
+var ErrJavaResolveFromEnvironment = errors.New("failed to resolve Java command from environment")
 
 func main() {
-
 	javaCmd := resolveJavaCmd()
 
-	err := checkJavaVersion(javaCmd)
+	err := exec.Command(javaCmd, "--version").Run() // check if the binary to this command exists
 	if err != nil {
-		printJavaConfigurationNotFound()
+		printJavaConfigNotFound()
+
 		return
 	}
 
 	montreeeDir := defaultMontreeeDir
 
-	if dir, isSet := os.LookupEnv(montreeeDirEnv); isSet {
+	if dir, ok := os.LookupEnv(envMontreeeDir); ok {
 		montreeeDir = dir
 	}
 
-	//todo decide on using a environment variable to add extra vm args
+	// todo decide on using a environment variable to add extra vm args
 	args := append([]string{"-cp", montreeeDir + montreeeClasspath, montreeeMainClass}, os.Args[1:]...)
 	montreeeCmd := exec.Command(javaCmd, args...)
 
-	if dir, isSet := os.LookupEnv(montreeeWorkingDirEnv); isSet {
+	if dir, ok := os.LookupEnv(envMontreeeWorkingDir); ok {
 		montreeeCmd.Dir = dir
 	}
 
 	montreeeCmd.Stdout = os.Stdout
 	montreeeCmd.Stderr = os.Stderr
-	_ = montreeeCmd.Run()
+
+	err = montreeeCmd.Run()
+	if err != nil {
+		panic(err)
+	}
 }
 
 func resolveJavaCmd() string {
 	javaCmd, err := resolveJavaCmdFromEnvironment()
 	if err != nil {
-		javaCmd = defaultJavaCmd
+		javaCmd = "java"
 	}
 	return javaCmd
 }
 
 func resolveJavaCmdFromEnvironment() (string, error) {
-	for _, env := range posibleJavaHomeDirEnvs {
-
+	for _, env := range javaHomeDirEnvs {
 		dir, isSet := os.LookupEnv(env)
 		if !isSet {
 			continue
 		}
 
-		for _, binDir := range posibleJavaExecuteables {
+		for _, binDir := range javaExecuteables {
+			checkCmd := filepath.FromSlash(dir + binDir)
 
-			javaCmdToCkeck := filepath.FromSlash(dir + binDir)
-
-			err := checkJavaVersion(javaCmdToCkeck)
+			err := exec.Command(checkCmd, "--version").Run()
 			if err != nil {
 				continue
 			}
 
-			return javaCmdToCkeck, nil
+			return checkCmd, nil
 		}
 	}
-	return "", NewErrFailedToResolveJavaCommandFromEnvironment()
+
+	return "", ErrJavaResolveFromEnvironment
 }
 
-type ErrFailedToResolveJavaCommandFromEnvironment struct{}
-
-func NewErrFailedToResolveJavaCommandFromEnvironment() *ErrFailedToResolveJavaCommandFromEnvironment {
-	return &ErrFailedToResolveJavaCommandFromEnvironment{}
-}
-
-func (e ErrFailedToResolveJavaCommandFromEnvironment) Error() string {
-	return "Failed to resolve Java command from environment."
-}
-
-func checkJavaVersion(javaCmd string) error {
-	cmd := exec.Command(javaCmd, "--version")
-	return cmd.Run()
-}
-
-func printJavaConfigurationNotFound() {
-	fmt.Println("Failed to find a valid java installation.")
-	fmt.Println("Try to add java to the path or set one of the following environment variables.")
+func printJavaConfigNotFound() {
+	fmt.Println("failed to find a valid java installation")
+	fmt.Println("try to add java to the path or set one of the following environment variables:")
 	fmt.Println()
-	for _, env := range posibleJavaHomeDirEnvs {
-		fmt.Println(env)
-	}
+	fmt.Println(strings.Join(javaHomeDirEnvs, "\n"))
 }
